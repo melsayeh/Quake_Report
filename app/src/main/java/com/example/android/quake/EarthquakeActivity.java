2f8dd0e788c147f86360966e076817599a0b5106
melsayeh <mansour.elsayeh@gmail.com>
2018-06-09 23:34:43 +0400

    Get JSON From URL

--- app/src/main/java/com/example/android/quake/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quake/EarthquakeActivity.java
@@ -15,16 +15,28 @@
  */
 package com.example.android.quake;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
+    public static final String WEBSITE = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2012-01-01&endtime=2017-12-01&minmagnitude=6"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,5 +55,61 @@ public class EarthquakeActivity extends AppCompatActivity {
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(adapter);
+    }
+
+    private class quakeAsyncTask extends AsyncTask<URL,Void,Earthquake>{
+
+        @Override
+        protected Earthquake doInBackground(URL ... urls){
+            URL url = null;
+            try {
+                url = new URL(WEBSITE);
+            } catch (MalformedURLException e) {
+                Log.e("Main Activity", "Error with the URL"+e);
+            }
+
+            String jsonResponse = makeHttpRequest(url);
+        }
+    }
+
+    private String makeHttpRequest(URL url) throws IOException {
+        String jsonResonse = "";
+        if (url==null)
+            return jsonResonse;
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.connect();
+            inputStream = urlConnection.getInputStream();
+            if (urlConnection.getResponseCode()==200)
+                jsonResonse = readFromStream(inputStream);
+
+        } catch (IOException e){
+            Log.e("Main Activity", "Error openning connection"+e);
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
+
+        finally{
+            urlConnection.disconnect();
+            inputStream.close();
+        }
+        return jsonResonse;
+    }
+
+    private String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream , Charset.forName("UTF-8"));
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+        String line = reader.readLine();
+        while (line != null){
+            builder.append(line);
+            line = reader.readLine();
+        }
     }
 }
